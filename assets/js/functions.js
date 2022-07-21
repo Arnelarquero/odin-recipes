496e7021c4d2c63536df9c6b749f866112dc2009
Arnelarquero <arn.arquero@gmail.com>
2022-07-22 02:34:04 +0800

    add reusable navigation for every page

---
+++ assets/js/functions.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+var menulist = document.getElementById("menulist");
+      
+    });
+
+      
+      function togglemenu(){
+        if(menulist.style.maxHeight== "0px"){
+          menulist.style.maxHeight = "130px";
+        }
+        else{
+          menulist.style.maxHeight = "0px";
+        }
+      }
+    
+
+      $.get("/navigation.html", function(data){
+        $("#nav-container").replaceWith(data);
+    });
+
